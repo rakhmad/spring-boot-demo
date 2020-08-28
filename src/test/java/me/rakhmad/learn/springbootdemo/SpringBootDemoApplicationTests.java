dfe4473f81f51d9a1b628196bc4f8d9740d03f99
Rakhmad Azhari <rakhmad.azhari@gmail.com>
2020-08-28 16:17:17 +0700

    Spring Boot Demo

---
+++ src/test/java/me/rakhmad/learn/springbootdemo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.rakhmad.learn.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
